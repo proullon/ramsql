28861e234269e25e68230f6fee2179afe5df67fc
Rierre Roullon <pierre.roullon@gmail.com>
2014-11-01 22:50:16 +0100

    Add stubs for createTable

--- engine/table.go
+++ engine/table.go
@@ -1,5 +1,12 @@
 package engine
 
+import (
+	"errors"
+	"fmt"
+
+	"github.com/proullon/ramsql/engine/parser"
+)
+
 type Field struct {
 	Name string
 	Type interface{}
@@ -8,4 +15,18 @@ type Field struct {
 type Table struct {
 	Name   string
 	Fields []Field
+}
+
+func createTableExecutor(e *Engine, i parser.Instruction) (string, error) {
+
+	if len(i.Decls) < 3 {
+		return "", errors.New("No table name provided")
+	}
+
+	t := Table{
+		Name: i.Decls[2].Lexeme,
+	}
+
+	e.tables[t.Name] = t
+	return fmt.Sprintf("table %s created", t.Name), nil
 }
--- engine/table_test.go
+++ engine/table_test.go
@@ -2,8 +2,39 @@ package engine
 
 import (
 	"testing"
+
+	"github.com/proullon/ramsql/engine/parser"
 )
 
 func TestCreateTable(t *testing.T) {
-	t.Fatal("Cannot create table")
+	query := `CREATE TABLE user
+	(
+        id INT PRIMARY KEY NOT NULL,
+	    last_name VARCHAR(100),
+	    first_name VARCHAR(100),
+	    email VARCHAR(255),
+	    birth_date DATE,
+	    country VARCHAR(255),
+	    town VARCHAR(255),
+	    zip_code VARCHAR(5)
+	)`
+
+	e, err := New()
+	if err != nil {
+		t.Fatalf("Cannot create new engine: %s", err)
+	}
+
+	i, err := parser.ParseInstruction(query)
+	if err != nil {
+		t.Fatalf("Cannot parse query %s : %s", query, err)
+	}
+
+	r, err := e.executeQuery(i[0])
+	if err != nil {
+		t.Fatalf("Cannot execute query: %s", err)
+	}
+
+	if r != "table test created" {
+		t.Fatalf("Query failed, expected TITIT, got %s", r)
+	}
 }
