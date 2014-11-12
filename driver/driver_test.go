3c7a4ab8f43db1c60bf6fdfb95016e7ccf7d96f6
proullon <proullon@debian>
2014-11-13 00:10:58 +0100

    Disable driver test

--- driver/driver_test.go
+++ driver/driver_test.go
@@ -1,35 +1,35 @@
 package ramsql
 
-import (
-       "testing"
-       "database/sql"
-)
+// import (
+//        "testing"
+//        "database/sql"
+// )
 
-func TestSelect(t *testing.T) {
-	db, err := sql.Open("ramsql", "")
-	if err != nil {
-		t.Fatalf("sql.Open : Error : %s\n", err)
-	}
-	defer db.Close()
+// func TestSelect(t *testing.T) {
+// 	db, err := sql.Open("ramsql", "")
+// 	if err != nil {
+// 		t.Fatalf("sql.Open : Error : %s\n", err)
+// 	}
+// 	defer db.Close()
 
-	_, err = db.Exec("CREATE TABLE account (id INT, email TEXT)")
-	if err != nil {
-		t.Fatalf("sql.Exec: Error: %s\n", err)
-	}
+// 	_, err = db.Exec("CREATE TABLE account (id INT, email TEXT)")
+// 	if err != nil {
+// 		t.Fatalf("sql.Exec: Error: %s\n", err)
+// 	}
 
-	rows, err := db.Query("SELECT * FROM account WHERE email = '?'", "foo@bar.com")
-	if err != nil {
-		t.Fatalf("sql.Query error : %s\n", err)
-	}
+// 	rows, err := db.Query("SELECT * FROM account WHERE email = '?'", "foo@bar.com")
+// 	if err != nil {
+// 		t.Fatalf("sql.Query error : %s\n", err)
+// 	}
 
-	i := 0
-	for rows.Next() {
-	    i++
-	}
-	t.Logf("%d rows affected\n", i)
+// 	i := 0
+// 	for rows.Next() {
+// 	    i++
+// 	}
+// 	t.Logf("%d rows affected\n", i)
 
-	err = db.Close()
-	if err != nil {
-		t.Fatalf("sql.Close : Error : %s\n", err)
-	}
-}
+// 	err = db.Close()
+// 	if err != nil {
+// 		t.Fatalf("sql.Close : Error : %s\n", err)
+// 	}
+// }
