4dfc63d78a9f310ffdf66e78bece14e76632bed8
Pierre Roullon <pierre.roullon@gmail.com>
2014-11-16 22:45:32 +0100

    Fix terminology

--- engine/table.go
+++ engine/table.go
@@ -8,60 +8,92 @@ import (
 	"github.com/proullon/ramsql/engine/parser"
 )
 
-type Attribute struct {
-	Name     string
-	TypeName string
-	Type     interface{}
+// Domain is the set of allowable values for an Attribute.
+type Domain struct {
 }
 
-type Row struct {
+// Attribute is a named column of a relation
+// AKA Field
+// AKA Column
+type Attribute struct {
+	name         string
+	typeName     string
+	typeInstance interface{}
+	defaultValue interface{}
+}
+
+// Tuple is a row in a relation
+type Tuple struct {
 	Values []interface{}
 }
 
-type Table struct {
-	Name       string
-	Attributes []Attribute
-	Rows       []Row
+// Relation is a table with column and rows
+// AKA File
+type Relation struct {
+	table Table
+	rows  []Tuple
 }
 
-func (t Table) String() string {
-	stringy := t.Name + " ("
-	for i, a := range t.Attributes {
-		if i != 0 {
-			stringy += " | "
-		}
-		stringy += a.Name + " " + a.TypeName
+type Table struct {
+	name       string
+	attributes []Attribute
+}
+
+func NewTable(name string) *Table {
+	t := &Table{
+		name: name,
 	}
-	stringy += ")"
-	return stringy
+
+	return t
+}
+
+func (t *Table) AddAttribute(attr Attribute) error {
+	// Check that name is not already taken
+	// for i := range t.attributes {
+
+	// }
+	// t.attributes = append(t.attributes, attr)
+	return nil
 }
 
 func (t *Table) Insert(values []interface{}) error {
 	return nil
 }
 
+func (t Table) String() string {
+	stringy := t.name + " ("
+	for i, a := range t.attributes {
+		if i != 0 {
+			stringy += " | "
+		}
+		stringy += a.name + " " + a.typeName
+	}
+	stringy += ")"
+	return stringy
+}
+
 func createTableExecutor(e *Engine, tableDecl *parser.Decl) (string, error) {
 
-	// Fetch table name
-	// if len(tableDecl.Decl) < 1 && tableDecl.Decl[0].Token != parser.StringToken {
-	// 	return "", errors.New("No table name provided")
+	// // Fetch table name
+	// // if len(tableDecl.Decl) < 1 && tableDecl.Decl[0].Token != parser.StringToken {
+	// // 	return "", errors.New("No table name provided")
+	// // }
+	// t := Table{
+	// 	Name: tableDecl.Decl[0].Lexeme,
 	// }
-	t := Table{
-		Name: tableDecl.Decl[0].Lexeme,
-	}
 
-	// Fetch attributes
-	for i := 1; i < len(tableDecl.Decl); i++ {
-		attr := Attribute{}
+	// // Fetch attributes
+	// for i := 1; i < len(tableDecl.Decl); i++ {
+	// 	attr := Attribute{}
 
-		attr.Name = tableDecl.Decl[i].Lexeme
-		attr.TypeName = tableDecl.Decl[i].Decl[0].Lexeme
+	// 	attr.Name = tableDecl.Decl[i].Lexeme
+	// 	attr.TypeName = tableDecl.Decl[i].Decl[0].Lexeme
 
-		t.Attributes = append(t.Attributes, attr)
-	}
+	// 	t.Attributes = append(t.Attributes, attr)
+	// }
 
-	e.tables[t.Name] = t
-	fmt.Println(t)
+	// e.tables[t.Name] = t
+	// fmt.Println(t)
 	return fmt.Sprintf("0 1"), nil
 }
 
