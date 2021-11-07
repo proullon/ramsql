aace95a25f52938ca1be2a6d69a97d0627cf09e2
Pierre Roullon <pierre.roullon@gmail.com>
2021-11-07 21:34:50 +0100

    fix: add altervative AUTO_INCREMENT lexeme

--- driver/autoincrement_test.go
+++ driver/autoincrement_test.go
@@ -49,3 +49,46 @@ func TestAutoIncrementSimple(t *testing.T) {
 		t.Fatalf("Last insterted id should be 2, not %d", lastID)
 	}
 }
+
+func TestAutoIncrementAlternativeSimple(t *testing.T) {
+	log.UseTestLogger(t)
+
+	db, err := sql.Open("ramsql", "TestAutoIncrementAlternativeSimple")
+	if err != nil {
+		t.Fatalf("sql.Open : Error : %s\n", err)
+	}
+	defer db.Close()
+
+	_, err = db.Exec("CREATE TABLE account (id INT AUTO_INCREMENT, email TEXT)")
+	if err != nil {
+		t.Fatalf("sql.Exec: Error: %s\n", err)
+	}
+
+	res, err := db.Exec("INSERT INTO account ('email') VALUES ('foo@bar.com')")
+	if err != nil {
+		t.Fatalf("Cannot insert into table account: %s", err)
+	}
+
+	lastID, err := res.LastInsertId()
+	if err != nil {
+		t.Fatalf("Cannot fetch last inserted id: %s\n", err)
+	}
+
+	if lastID != 1 {
+		t.Fatalf("Last insterted id should be 1, not %d", lastID)
+	}
+
+	res, err = db.Exec("INSERT INTO account ('email') VALUES ('roger@gmail.com')")
+	if err != nil {
+		t.Fatalf("Cannot insert into table account: %s", err)
+	}
+
+	lastID, err = res.LastInsertId()
+	if err != nil {
+		t.Fatalf("Cannot fetch last inserted id: %s\n", err)
+	}
+
+	if lastID != 2 {
+		t.Fatalf("Last insterted id should be 2, not %d", lastID)
+	}
+}
