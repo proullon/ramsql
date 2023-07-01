e76bcdd7280edbe970cde4fd220214d57338f01d
Pierre Roullon <pierre.roullon@gmail.com>
2023-07-01 22:41:28 +0200

    chore: add test with gorm

---
+++ driver/gorm_test.go
@@ -0,0 +1,52 @@
+package ramsql
+
+import (
+	"database/sql"
+	"testing"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type Product struct {
+	gorm.Model
+	Code  string
+	Price uint
+}
+
+// From https://gorm.io/docs/connecting_to_the_database.html
+// and  https://gorm.io/docs/
+func TestGormQuickStart(t *testing.T) {
+	ramdb, err := sql.Open("ramsql", "TestGormQuickStart")
+	if err != nil {
+		t.Fatalf("cannot open db: %s", err)
+	}
+
+	db, err := gorm.Open(postgres.New(postgres.Config{
+		Conn: ramdb,
+	}),
+		&gorm.Config{})
+	if err != nil {
+		t.Fatalf("cannot setup gorm: %s", err)
+	}
+
+	// Migrate the schema
+	db.AutoMigrate(&Product{})
+
+	// Create
+	db.Create(&Product{Code: "D42", Price: 100})
+
+	// Read
+	var product Product
+	db.First(&product, 1)                 // find product with integer primary key
+	db.First(&product, "code = ?", "D42") // find product with code D42
+
+	// Update - update product's price to 200
+	db.Model(&product).Update("Price", 200)
+	// Update - update multiple fields
+	db.Model(&product).Updates(Product{Price: 200, Code: "F42"}) // non-zero fields
+	db.Model(&product).Updates(map[string]interface{}{"Price": 200, "Code": "F42"})
+
+	// Delete - delete product
+	db.Delete(&product, 1)
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,22 @@ module github.com/proullon/ramsql
 go 1.20
 
 require (
-	github.com/go-gorp/gorp v2.0.0+incompatible
-	github.com/lib/pq v1.0.0
+	github.com/go-gorp/gorp v2.2.0+incompatible
+	github.com/lib/pq v1.10.9
+	gorm.io/driver/postgres v1.5.2
+	gorm.io/gorm v1.25.2
+)
+
+require (
+	github.com/go-sql-driver/mysql v1.7.1 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgx/v5 v5.3.1 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/mattn/go-sqlite3 v1.14.17 // indirect
+	github.com/poy/onpar v0.3.2 // indirect
+	github.com/ziutek/mymysql v1.5.4 // indirect
+	golang.org/x/crypto v0.8.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
 )
