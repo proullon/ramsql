e6abb172ff3b7fb9603eec941e202b63400dc35c
Pierre Roullon <pierre.roullon@gmail.com>
2023-07-14 18:10:10 +0200

    chore: set benchmark baseline

--- bench_test.go
+++ bench_test.go
@@ -70,7 +70,7 @@ func benchmarkInsert(b *testing.B, db *sql.DB, nbRows int) {
 	}
 }
 
-func benchmarkSelect(b *testing.B, db *sql.DB) {
+func benchmarkSelectBTree(b *testing.B, db *sql.DB) {
 
 	var id int64
 	var email string
@@ -100,18 +100,48 @@ func benchmarkSelect(b *testing.B, db *sql.DB) {
 	}
 }
 
-func BenchmarkRamSQLSelect(b *testing.B) {
-	db, err := sql.Open("ramsql", "BenchmarkSQLSelect")
+func benchmarkSelectHashMap(b *testing.B, db *sql.DB) {
+
+	var id int64
+	var email string
+
+	b.ResetTimer()
+	for n := 0; n < b.N; n++ {
+		query := `SELECT account.id, account.email FROM account WHERE id = $1`
+		rows, err := db.Query(query, n)
+		if err != nil {
+			b.Fatalf("cannot query rows: %s", err)
+		}
+
+		for rows.Next() {
+			err = rows.Scan(&id, &email)
+			if err != nil {
+				b.Fatalf("cannot scan rows: %s", err)
+			}
+		}
+	}
+
+	_ = id
+	_ = email
+
+	_, err := db.Exec(`DROP TABLE account`)
+	if err != nil {
+		b.Fatalf("sql.Exec: %s", err)
+	}
+}
+
+func BenchmarkRamSQLSelectBTree(b *testing.B) {
+	db, err := sql.Open("ramsql", "BenchmarkSQLSelectBTree")
 	if err != nil {
 		b.Fatalf("cannot open ramsql db")
 	}
 
 	n := 100
 	setupInsertN(b, db, n)
-	benchmarkSelect(b, db)
+	benchmarkSelectBTree(b, db)
 }
 
-func BenchmarkSQLiteSelect(b *testing.B) {
+func BenchmarkSQLiteSelectBTree(b *testing.B) {
 	db, err := sql.Open("sqlite", ":memory:")
 	if err != nil {
 		b.Fatalf("cannot open sqlite")
@@ -119,21 +149,21 @@ func BenchmarkSQLiteSelect(b *testing.B) {
 
 	n := 100
 	setupInsertN(b, db, n)
-	benchmarkSelect(b, db)
+	benchmarkSelectBTree(b, db)
 }
 
-func BenchmarkRamSQLSelect10K(b *testing.B) {
-	db, err := sql.Open("ramsql", "BenchmarkSQLSelect1M")
+func BenchmarkRamSQLSelectBTree10K(b *testing.B) {
+	db, err := sql.Open("ramsql", "BenchmarkSQLSelectBTree10K")
 	if err != nil {
 		b.Fatalf("cannot open ramsql db")
 	}
 
 	n := 10000
 	setupInsertN(b, db, n)
-	benchmarkSelect(b, db)
+	benchmarkSelectBTree(b, db)
 }
 
-func BenchmarkSQLiteSelect10K(b *testing.B) {
+func BenchmarkSQLiteSelectBTree10K(b *testing.B) {
 	db, err := sql.Open("sqlite", ":memory:")
 	if err != nil {
 		b.Fatalf("cannot open sqlite")
@@ -141,11 +171,77 @@ func BenchmarkSQLiteSelect10K(b *testing.B) {
 
 	n := 10000
 	setupInsertN(b, db, n)
-	benchmarkSelect(b, db)
+	benchmarkSelectBTree(b, db)
+}
+
+func BenchmarkRamSQLSelectHashMap10K(b *testing.B) {
+	db, err := sql.Open("ramsql", "BenchmarkSQLSelectHashMap10K")
+	if err != nil {
+		b.Fatalf("cannot open ramsql db")
+	}
+
+	n := 10000
+	setupInsertN(b, db, n)
+	benchmarkSelectHashMap(b, db)
+}
+
+func BenchmarkSQLiteSelectHashMap10K(b *testing.B) {
+	db, err := sql.Open("sqlite", ":memory:")
+	if err != nil {
+		b.Fatalf("cannot open sqlite")
+	}
+
+	n := 10000
+	setupInsertN(b, db, n)
+	benchmarkSelectHashMap(b, db)
+}
+
+func BenchmarkRamSQLSelectBTree100K(b *testing.B) {
+	db, err := sql.Open("ramsql", "BenchmarkSQLSelectBTree100K")
+	if err != nil {
+		b.Fatalf("cannot open ramsql db")
+	}
+
+	n := 100000
+	setupInsertN(b, db, n)
+	benchmarkSelectBTree(b, db)
+}
+
+func BenchmarkSQLiteSelectBTree100K(b *testing.B) {
+	db, err := sql.Open("sqlite", ":memory:")
+	if err != nil {
+		b.Fatalf("cannot open sqlite")
+	}
+
+	n := 100000
+	setupInsertN(b, db, n)
+	benchmarkSelectBTree(b, db)
+}
+
+func BenchmarkRamSQLSelectHashMap100K(b *testing.B) {
+	db, err := sql.Open("ramsql", "BenchmarkSQLSelectHashMap100K")
+	if err != nil {
+		b.Fatalf("cannot open ramsql db")
+	}
+
+	n := 100000
+	setupInsertN(b, db, n)
+	benchmarkSelectHashMap(b, db)
+}
+
+func BenchmarkSQLiteSelectHashMap100K(b *testing.B) {
+	db, err := sql.Open("sqlite", ":memory:")
+	if err != nil {
+		b.Fatalf("cannot open sqlite")
+	}
+
+	n := 100000
+	setupInsertN(b, db, n)
+	benchmarkSelectHashMap(b, db)
 }
 
 func BenchmarkRamSQLInsert10(b *testing.B) {
-	db, err := sql.Open("ramsql", "BenchmarkSQLSelect")
+	db, err := sql.Open("ramsql", "BenchmarkSQLSelectBTree")
 	if err != nil {
 		b.Fatalf("cannot open ramsql db")
 	}
@@ -162,7 +258,7 @@ func BenchmarkSQLiteInsert10(b *testing.B) {
 
 func BenchmarkRamSQLSetup(b *testing.B) {
 	for n := 0; n < b.N; n++ {
-		db, err := sql.Open("ramsql", "BenchmarkSQLSelect")
+		db, err := sql.Open("ramsql", "BenchmarkSQLSelectBTree")
 		if err != nil {
 			b.Fatalf("cannot open ramsql db")
 		}
