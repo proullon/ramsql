d851e3f8ea466e7ba8aa02c7c152ab99a570903c
Pierre Roullon <pierre.roullon@gmail.com>
2023-08-14 09:28:18 +0200

    feat: extend transaction testing

--- driver/tx_test.go
+++ driver/tx_test.go
@@ -2,7 +2,10 @@ package ramsql
 
 import (
 	"database/sql"
+	"sync"
 	"testing"
+
+	"github.com/proullon/ramsql/engine/log"
 )
 
 func TestTransaction(t *testing.T) {
@@ -30,6 +33,19 @@ func TestTransaction(t *testing.T) {
 		}
 	}
 
+	db.SetMaxOpenConns(10)
+	var wg sync.WaitGroup
+
+	for i := 0; i < 15; i++ {
+		wg.Add(1)
+		go execTestTransactionQuery(t, db, &wg)
+	}
+
+	wg.Wait()
+}
+
+func execTestTransactionQuery(t *testing.T, db *sql.DB, wg *sync.WaitGroup) {
+
 	tx, err := db.Begin()
 	if err != nil {
 		t.Fatalf("Cannot create tx: %s", err)
@@ -50,7 +66,75 @@ func TestTransaction(t *testing.T) {
 		t.Fatalf("cannot commit tx: %s", err)
 	}
 
-	// Select count
+	wg.Done()
+}
+
+func TestTransactionRollback(t *testing.T) {
+	log.SetLevel(log.InfoLevel)
+	defer log.SetLevel(log.ErrorLevel)
+
+	db, err := sql.Open("ramsql", "TestTransactionRollback")
+	if err != nil {
+		t.Fatalf("sql.Open : Error : %s\n", err)
+	}
+	defer db.Close()
+
+	init := []string{
+		`CREATE TABLE account (id INT, email TEXT)`,
+		`INSERT INTO account (id, email) VALUES (1, 'foo@bar.com')`,
+		`INSERT INTO account (id, email) VALUES (2, 'bar@bar.com')`,
+		`CREATE TABLE champion (user_id INT, name TEXT)`,
+		`INSERT INTO champion (user_id, name) VALUES (1, 'zed')`,
+		`INSERT INTO champion (user_id, name) VALUES (2, 'lulu')`,
+		`INSERT INTO champion (user_id, name) VALUES (1, 'thresh')`,
+		`INSERT INTO champion (user_id, name) VALUES (1, 'lux')`,
+	}
+	for _, q := range init {
+		_, err = db.Exec(q)
+		if err != nil {
+			t.Fatalf("sql.Exec: Error: %s\n", err)
+		}
+	}
+
+	var count int
+	err = db.QueryRow("SELECT COUNT(user_id) FROM champion WHERE user_id = 1").Scan(&count)
+	if err != nil {
+		t.Fatalf("cannot query row in tx: %s\n", err)
+	}
+	if count != 3 {
+		t.Fatalf("expected COUNT(user_id)=3 row, got %d", count)
+	}
+
+	tx, err := db.Begin()
+	if err != nil {
+		t.Fatalf("cannot begin transaction: %s", err)
+	}
+
+	_, err = tx.Exec(`INSERT INTO champion (user_id, name) VALUES (1, 'new-champ')`)
+	if err != nil {
+		t.Fatalf("cannot insert within transaction: %s", err)
+	}
+
+	err = tx.QueryRow("SELECT COUNT(*) FROM champion WHERE user_id = 1").Scan(&count)
+	if err != nil {
+		t.Fatalf("cannot query row in tx: %s\n", err)
+	}
+	if count != 4 {
+		t.Fatalf("expected COUNT(user_id)=4 row within tx, got %d", count)
+	}
+
+	err = tx.Rollback()
+	if err != nil {
+		t.Fatalf("cannot rollback transaction: %s", err)
+	}
+
+	err = db.QueryRow("SELECT COUNT(user_id) FROM champion WHERE user_id = 1").Scan(&count)
+	if err != nil {
+		t.Fatalf("cannot query row in tx: %s\n", err)
+	}
+	if count != 3 {
+		t.Fatalf("expected COUNT(user_id)=3 row, got %d", count)
+	}
 }
 
 func TestCheckAttributes(t *testing.T) {
